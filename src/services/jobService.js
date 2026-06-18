1dcfe4da597a56879d9b42ffea38da77ffafbfc6
CNDung24 <dungcnhe181056@fpt.edu.vn>
2026-06-18 11:07:06 +0700

    push code

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -102,6 +102,14 @@ export const getExperienceLevels = async () => {
 };
 
 /**
+ * Lấy danh sách Khoảng lương mặc định (master data)
+ */
+export const getSalaryRanges = async () => {
+  const response = await api.get('/master-data/salary-ranges');
+  return response.data;
+};
+
+/**
  * Lấy danh sách Kỹ năng theo Nhóm ngành nghề
  */
 export const getSkillsByCareerGroup = async (careerGroupId) => {
